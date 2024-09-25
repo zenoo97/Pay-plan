3f7b735cbfb398a96081db78e91f86785ddbda98
zenoo97 <jaehun4086@naver.com>
2024-09-25 13:23:58 +0900

    MakeChallenge 컴포넌트 로직 및 디자인 수정

--- client/src/components/home/MakeChallengeComponent.tsx
+++ client/src/components/home/MakeChallengeComponent.tsx
@@ -1,21 +1,32 @@
 import {useState} from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {colors} from '../../color';
 import DatePicker from 'react-native-date-picker';
 import {supabase} from '../../lib/supabase';
 import {useNavigation} from '@react-navigation/native';
 import {useUserStore} from '../../store/getUser';
+import React from 'react';
+import GoBackBtn from '../../shared/GoBackBtn';
 
 function MakeChallengeComponent({userData}) {
   const navigation = useNavigation();
   const [challengeName, setChallengeName] = useState('');
-  const [goalPeriodStart, setGoalPeriodStart] = useState('');
-  const [goalPrice, setGoalPrice] = useState('');
-  const [userId, setUserId] = useState('');
-  const [userPassword, setUserPassWord] = useState('');
+  const [goalPrice, setGoalPrice] = useState(0);
   const [date, setDate] = useState(new Date());
-  const [open, setOpen] = useState(false);
+  const [goalStartDate, setGoalStartDate] = useState(new Date());
+  const [goalEndDate, setGoalEndDate] = useState(new Date());
+  const [goalStartOpen, setGoalStartOpen] = useState(false);
+  const [goalEndOpen, setGoalEndOpen] = useState(false);
   const addMakedChallenge = useUserStore(state => state.addMakedChallenge);
+  let startDate, endDate;
   const addChallenge = async () => {
     try {
       const {data, error} = await supabase
@@ -23,10 +34,10 @@ function MakeChallengeComponent({userData}) {
         .insert([
           {
             challenge_name: challengeName,
-            goal_price: goalPrice,
+            goal_price: Number(goalPrice),
             user_id: userData[0].user_id,
-            goal_period_start: date.toLocaleDateString('ko-KR'),
-            goal_period_end: date.toLocaleDateString('ko-KR'),
+            goal_period_start: goalStartDate.toISOString().split('T')[0],
+            goal_period_end: goalEndDate.toISOString().split('T')[0],
           },
         ])
         .select();
@@ -63,10 +74,13 @@ function MakeChallengeComponent({userData}) {
   return (
     <View style={styles.container}>
       <View>
+        <View>
+          <GoBackBtn />
+        </View>
         <Text style={styles.challengeAddText}>챌린지 추가</Text>
       </View>
       <View style={styles.infoContainer}>
-        <View>
+        <View style={styles.challengeNameiInfo}>
           <View>
             <Text style={styles.inputTitleText}>챌린지 이름</Text>
           </View>
@@ -75,54 +89,90 @@ function MakeChallengeComponent({userData}) {
               style={styles.goalPriceInput}
               value={challengeName}
               onChangeText={setChallengeName}
+              placeholder="챌린지 이름을 입력해주세요."
+              maxLength={15}
             />
           </View>
         </View>
-        <View>
+        <View style={styles.inputInfo}>
           <View>
             <Text style={styles.inputTitleText}>목표 기간</Text>
           </View>
-          <View>
+          <View style={styles.datePicker}>
             <View>
-              <Button title="Open" onPress={() => setOpen(true)} />
+              <TouchableOpacity
+                style={styles.dateInput}
+                onPress={() => setGoalStartOpen(true)}>
+                <Text>
+                  {startDate !== ''
+                    ? goalStartDate.toISOString().split('T')[0]
+                    : '시작일'}
+                </Text>
+              </TouchableOpacity>
               <DatePicker
                 modal
-                open={open}
-                date={date}
+                open={goalStartOpen}
+                date={goalStartDate}
                 onConfirm={date => {
-                  setOpen(false);
-                  setDate(date);
+                  setGoalStartOpen(false);
+                  setGoalStartDate(date);
+                  startDate = goalStartDate.toISOString().split('T')[0];
                 }}
                 onCancel={() => {
-                  setOpen(false);
+                  setGoalStartOpen(false);
                 }}
                 mode="date"
                 locale="ko-KR"
               />
             </View>
             <View>
-              <TextInput style={styles.goalPeriodEndInput} value={date} />
+              <TouchableOpacity
+                style={styles.dateInput}
+                onPress={() => setGoalEndOpen(true)}>
+                <Text>
+                  {endDate !== ''
+                    ? goalEndDate.toISOString().split('T')[0]
+                    : '종료일'}
+                </Text>
+              </TouchableOpacity>
+              <DatePicker
+                modal
+                open={goalEndOpen}
+                date={goalEndDate}
+                onConfirm={date => {
+                  setGoalEndOpen(false);
+                  setGoalEndDate(date);
+                  endDate = goalEndDate.toISOString().split('T')[0];
+                }}
+                onCancel={() => {
+                  setGoalEndOpen(false);
+                }}
+                mode="date"
+                locale="ko-KR"
+              />
             </View>
           </View>
-          <View>
-            <TextInput />
-          </View>
         </View>
-        <View>
+        <View style={styles.goalPriceInfo}>
           <View>
             <Text style={styles.inputTitleText}>목표 금액</Text>
           </View>
           <View>
             <TextInput
               style={styles.goalPriceInput}
-              value={goalPrice}
-              onChangeText={setGoalPrice}
+              value={goalPrice ? Number(goalPrice).toLocaleString() : ''}
+              onChangeText={text => setGoalPrice(text.replace(/[^0-9]/g, ''))}
+              placeholder="목표 금액을 입력해주세요."
+              maxLength={15}
+              keyboardType="numeric"
             />
           </View>
         </View>
-        <View style={styles.endBtn}>
-          <Button title="추가하기" onPress={addChallenge} />
-        </View>
+      </View>
+      <View style={styles.endBtn}>
+        <TouchableOpacity style={styles.addBtn} onPress={addChallenge}>
+          <Text style={styles.addBtnText}>완료</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -134,24 +184,64 @@ const styles = StyleSheet.create({
   challengeAddText: {
     fontSize: 20,
     color: colors.blueText,
+    paddingHorizontal: 10,
   },
   inputTitleText: {
     color: colors.blackText,
     fontSize: 20,
     fontWeight: 'bold',
   },
-  infoContainer: {
+  datePicker: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  dateInput: {
+    width: 170,
+    height: 50,
+    borderWidth: 2,
+    borderColor: colors.inputGreyColor,
     alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  infoContainer: {
+    padding: 10,
+    gap: 50,
+    alignItems: 'center',
+  },
+  challengeNameiInfo: {
+    gap: 10,
   },
   goalPeriodStartInput: {},
   goalPeriodEndInput: {},
   goalPriceInput: {
     width: 356,
-    height: 40,
     borderRadius: 10,
     backgroundColor: colors.inputGreyColor,
   },
+  inputInfo: {
+    gap: 20,
+  },
+  goalPriceInfo: {
+    gap: 10,
+  },
   endBtn: {
-    width: 100,
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  addBtn: {
+    width: 133,
+    height: 46,
+    backgroundColor: colors.blueText,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addBtnText: {
+    fontSize: 20,
+    color: colors.input,
+    fontWeight: 'bold',
   },
 });
