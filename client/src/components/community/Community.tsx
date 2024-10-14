9f851fade442e94959a84d7e3725729cdcff09dc
zenoo97 <jaehun4086@naver.com>
2024-10-14 17:31:43 +0900

    [Update] communiuy 컴포넌트 구현
    - 성공, 실패시 상태별로 컴포넌트 데이터 뿌리기

--- client/src/components/community/Community.tsx
+++ client/src/components/community/Community.tsx
@@ -1,21 +1,99 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import UserWritePost from './UserWritePost';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+import {colors} from '../../color';
+import {height, width} from '../../shared/phoneSize';
+import SuccessPostComponent from './SuccessPostComponent';
+import FailPostComponent from './failPostComponent';
 
 function Community() {
+  let [btnStatus, setBtnStaus] = useState(false);
+  let [failData, setFailData] = useState([]);
+  const successBtnHandler = () => {
+    setBtnStaus(false);
+  };
+  const failBtnHandler = () => {
+    setBtnStaus(true);
+  };
+
   return (
     <View style={styles.container}>
-      <UserWritePost />
-      <UserWritePost />
-      <UserWritePost />
+      <View style={styles.selectBtn}>
+        <View style={styles.successBtnContainer}>
+          <TouchableOpacity
+            style={[btnStatus ? styles.defaultBtn : styles.successBtn]}
+            onPress={successBtnHandler}>
+            <Text
+              style={[
+                btnStatus ? styles.defaultBtnText : styles.successBtnText,
+              ]}>
+              성공
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.failBtnContainer}>
+          <TouchableOpacity
+            style={[!btnStatus ? styles.defaultBtn : styles.successBtn]}
+            onPress={failBtnHandler}>
+            <Text
+              style={[
+                !btnStatus ? styles.defaultBtnText : styles.successBtnText,
+              ]}>
+              실패
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {btnStatus === false ? (
+        <>
+          <View style={styles.userInfo}>
+            <SuccessPostComponent />
+          </View>
+        </>
+      ) : (
+        <>
+          <View style={styles.userInfo}>
+            <FailPostComponent />
+          </View>
+        </>
+      )}
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 10,
     paddingVertical: 5,
   },
+  selectBtn: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  successBtnContainer: {
+    flex: 1,
+  },
+  failBtnContainer: {
+    flex: 1,
+  },
+  defaultBtn: {
+    backgroundColor: colors.inputGreyColor,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 35 * height,
+  },
+  successBtn: {
+    backgroundColor: colors.blueText,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 35 * height,
+  },
+  defaultBtnText: {
+    color: colors.blackText,
+  },
+  successBtnText: {
+    color: 'white',
+  },
+  userInfo: {},
 });
 export default Community;
---
+++ client/src/components/community/FailPostComponent.tsx
@@ -0,0 +1,138 @@
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
+import {height, scale, width} from '../../shared/phoneSize';
+import {colors} from '../../color';
+import React, {useEffect, useState} from 'react';
+import {supabase} from '../../lib/supabase';
+
+const FailPostComponent = () => {
+  let [failData, setFailData] = useState([]);
+  const getUserPostData = async () => {
+    let {data: users_post, error} = await supabase
+      .from('users_post')
+      .select('*')
+      .eq('status', false);
+
+    if (error) {
+      console.log('데이터 가져오기 오류:', error.message);
+      return;
+    }
+
+    console.log('실패 결과', users_post);
+
+    // 상태에 모든 데이터를 저장
+    setFailData(users_post);
+  };
+  useEffect(() => {
+    getUserPostData();
+  }, []);
+  let {
+    id,
+    challenge_name,
+    challenge_review,
+    favorite,
+    user_name,
+    user_nickname,
+  } = failData;
+  console.log(failData);
+  return (
+    <View>
+      {failData.map(item => (
+        <View key={item.id} style={styles.infoContainer}>
+          <View>
+            <View style={styles.userInfo}>
+              <View>
+                <View style={styles.profile}></View>
+              </View>
+              <View>
+                <View>
+                  <Text style={styles.idText}>{item.user_name}</Text>
+                </View>
+                <View>
+                  <Text>@{item.user_nickname}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.postInfo}>
+            <View>
+              <View>
+                <Text style={styles.challengeName}>
+                  챌린지명: {item.challenge_name}
+                </Text>
+              </View>
+              <View>
+                <Text style={styles.reviewText}>
+                  실패 원인: {item.challenge_review}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.cheerUp}>
+              <View>
+                <Image
+                  source={require('../../../public/images/thumb_up.png')}
+                />
+              </View>
+              <View>
+                <TouchableOpacity>
+                  <Text>응원하기</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    borderRadius: 10 * width,
+    padding: 40 * width,
+    gap: 10,
+    marginBottom: 10 * height,
+    borderBottomWidth: 1,
+  },
+  userInfo: {
+    alignItems: 'center',
+    width: '100%',
+    flexDirection: 'row',
+    gap: 10 * width,
+  },
+  profile: {
+    backgroundColor: 'grey',
+    width: 50 * width,
+    height: 35 * height,
+    borderRadius: 100 * width,
+  },
+  idText: {
+    fontSize: 20 * scale,
+    fontWeight: 'bold',
+    color: colors.blackText,
+  },
+  postInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  challengeName: {
+    fontSize: 20 * scale,
+    color: colors.blackText,
+    fontWeight: 'bold',
+  },
+  reviewText: {
+    fontSize: 15 * scale,
+    fontWeight: 'bold',
+    color: colors.blackText,
+  },
+  cheerUp: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E4F1FF',
+    borderRadius: 10 * width,
+    padding: 10,
+  },
+});
+
+export default FailPostComponent;
