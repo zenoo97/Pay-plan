0cd713f40282bea61a823ed163ac7a1306c80b1c
zenoo97 <jaehun4086@naver.com>
2024-10-24 15:35:08 +0900

    [Update] list컴포넌트 총 사용금액

--- client/src/color.ts
+++ client/src/color.ts
@@ -3,6 +3,7 @@ export const colors = {
   input: '#ECECEC',
   blueText: '#1AA9D6',
   blackText: '#4A4A4A',
+  downBlue: '#F1FAFD',
   inputGreyColor: '#ECECEC',
   mintColor: '#5ACCAA',
 };
--- client/src/components/list/ListComponent.tsx
+++ client/src/components/list/ListComponent.tsx
@@ -3,6 +3,7 @@ import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
 import {useUserStore} from '../../store/getUser';
 import {colors} from '../../color';
 import DropDown from '../../shared/DropDown';
+import {height, width} from '../../shared/phoneSize';
 
 const ListCom = memo(({date, title, used_price}) => {
   const formatDate = dateString => {
@@ -27,7 +28,7 @@ const ListCom = memo(({date, title, used_price}) => {
   return (
     <View style={styles.usedInfo}>
       <View style={styles.dateInfo}>
-        <View>
+        <View style={styles.dateInfoCon}>
           <Text>{formattedDate}</Text>
         </View>
         <View>
@@ -50,14 +51,29 @@ function ListComponent({userData}) {
   const selectedChallengeList = useUserStore(
     state => state.selectedChallengeList,
   );
+  const [totalUsedMoney, setTotalUsedMoney] = useState(0);
+  const totalMoneyCalc = () => {
+    let value = selectedChallengeList.map(item => Number(item.used_price));
+    let summary = value.reduce((a, b) => a + b, 0);
+    setTotalUsedMoney(summary);
+  };
   // console.log(userChallengeList, '챌린지 리스트 in list컴포넌트');
   // console.log(userChallengeList, 'in ListComponent');
 
-  useEffect(() => {}, [selectedChallengeList]);
+  useEffect(() => {
+    totalMoneyCalc();
+  }, [selectedChallengeList]);
   return (
     <View style={styles.container}>
       <View style={styles.usedList}>
         <DropDown />
+      </View>
+      <View style={styles.totalUsedMoney}>
+        <View style={styles.totalMoney}>
+          <Text style={styles.totalMoneyText}>
+            총 사용 금액 : {totalUsedMoney.toLocaleString()}원
+          </Text>
+        </View>
       </View>
       <FlatList
         data={selectedChallengeList}
@@ -83,6 +99,28 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 20,
   },
+  totalUsedMoney: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    width: '100%',
+  },
+  totalMoney: {
+    width: 400 * width,
+    height: 50 * height,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.downBlue,
+    borderRadius: 10 * width,
+  },
+  totalMoneyText: {
+    color: colors.blueText,
+    textAlign: 'center',
+    width: 300 * width,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+
   usedListText: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -102,6 +140,9 @@ const styles = StyleSheet.create({
   dateInfo: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  dateInfoCon: {
+    width: 130 * width,
   },
   titleContainer: {
     flex: 1,
